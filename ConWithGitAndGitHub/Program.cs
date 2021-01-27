09a4b3e9085ddb2e9e75b5918bd8d5bd0e1d2c27
Jahnavi Kaparati <jahnavi.kaparati@zensar.com>
2021-01-27 06:01:51 +0530

    Add project files.

---
+++ ConWithGitAndGitHub/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConWithGitAndGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Person pobj = new Person { Id = 100, Name = "Deptesh" };
+            Console.ReadLine();
+        }
+    }
+}
